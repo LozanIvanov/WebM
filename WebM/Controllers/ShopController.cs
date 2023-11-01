53cde1faf684b9016ee20ba0dc6c50f21b417884
LozanIvanov <Lozan.Ivanov@abv.bg>
2023-11-01 11:18:11 +0200

    Create paging at Shop page. ShopController index method.Views shop index

--- WebM/Controllers/ShopController.cs
+++ WebM/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.Dal.Models.Admin;
 using Web.Dal.Services;
 
 namespace WebM.Controllers
@@ -6,23 +7,28 @@ namespace WebM.Controllers
     public class ShopController : Controller
     {
 
-        private readonly ProductService service;
+        private readonly ProductService productService;
         public ShopController(ProductService service)
         {
-            this.service = service;
+            this.productService = service;
         }
         [Route("/Shop/Details/{id}")]
         public IActionResult Details(int id)
         {
-            var product = this.service.GetProductById(id);
+            var product = this.productService.GetProductById(id);
 
             return View("~/Views/Shop/Details.cshtml", product);
         }
         public IActionResult Index(int? page,decimal minPrice = 0, decimal maxPrice = 0)
         {
-            var products = this.service.GetProducts( page,minPrice, maxPrice);
+            var result = new ProductViewModel()
+            {
+                ListProducts = this.productService.GetProducts(page),
+                TotalPages = this.productService.GetTotalPages(),
+                CurrentPage = page != null ? page.Value : 1
+            };
 
-            return View("~/Views/Shop/index.cshtml", products);
+            return View("~/Views/Shop/index.cshtml", result);
         }
     }
 }
